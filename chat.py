aefcbac68f6e82a152bb3eea20d039e43be465cc
4N1Z <20am014@sctce.ac.in>
2023-09-24 12:03:47 +0530

    persistant db added

--- chat.py
+++ chat.py
@@ -1,5 +1,9 @@
-from langchain.chains import RetrievalQAWithSourcesChain
+import os
+import streamlit as st
+from typing import Literal
 from dataclasses import dataclass
+
+from langchain.chains import RetrievalQAWithSourcesChain, ConversationalRetrievalChain, ConversationChain
 from langchain.embeddings.cohere import CohereEmbeddings
 from langchain.llms import Cohere
 from langchain.prompts import PromptTemplate
@@ -7,12 +11,16 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.conversation.memory import ConversationSummaryMemory
 
 from langchain.vectorstores import Qdrant
-import os
-import streamlit as st
-from typing import Literal
 from langchain.document_loaders import WebBaseLoader
+from qdrant_client import models, QdrantClient
 
-os.environ["COHERE_API_KEY"] = "qRMUuF5mmpHClOpnpk5BXsxUvzcnMFmQTUc6Gwh5"
+
+
+os.environ["COHERE_API_KEY"] = st.secrets["COHERE_API_KEY"]
+QDRANT_HOST = st.secrets["QDRANT_HOST"]
+QDRANT_API_KEY = st.secrets["QDRANT_API_KEY"]
+
+# "qRMUuF5mmpHClOpnpk5BXsxUvzcnMFmQTUc6Gwh5"
 st.set_page_config(page_title="Co:Chat - An LLM-powered chat bot")
 st.title("Sheraton-Bot")
 
@@ -41,20 +49,26 @@ def load_css():
 #              "https://www.expedia.co.in/Kochi-Hotels-Four-Points-By-Sheraton-Kochi-Infopark.h33351573.Hotel-Information",
 #              ] 
 
-web_links = ["https://hotels-ten.vercel.app/api/hotels"] 
-
-loader = WebBaseLoader(web_links)
-document=loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-texts = text_splitter.split_documents(document)
 
 embeddings = CohereEmbeddings(model = "embed-english-v2.0")
 print(" embedding docs !")
 
-vector_store = Qdrant.from_documents(texts, embeddings, location=":memory:",collection_name="summaries-po", distance_func="Dot")
-retriever=vector_store.as_retriever()
+# creating a client to connect to qdrant
+qdrant_client = QdrantClient(
+    url = QDRANT_HOST,
+    api_key= QDRANT_API_KEY,
+)
 
+# creating vector store
+collection_name = "hotelDataCollection"
+vector_store = Qdrant(
+    client=qdrant_client,
+    collection_name = collection_name,
+    embeddings=embeddings
+)
+print("connection established !")
+
+# vector_store = Qdrant.from_documents(texts, embeddings, location=":memory:",collection_name="summaries-po", distance_func="Dot")
 
 #initializing Session State
 def initialize_session_state() :
@@ -71,52 +85,40 @@ def initialize_session_state() :
     if "history" not in st.session_state:
         st.session_state.history = []
 
-    #    if "token_count" not in st.session_state:
-    #        st.session_state.token_count = 0
-
     if "chain" not in st.session_state :  
 
         #create custom prompt for your use case
         prompt_template = """
-        You are a Hotel Receptionist at "Four Points by Sheraton" hotel. You answer all the customer queries about the hotel and also make bookings at the hotel.
-        You are not only kind, helpful, polite and action-oriented, but also patient and empathetic, understanding the stress and confusion of customers. You are flexible and adapt your guidance to match the customer's level of knowledge and comfort. 
-        You do not bore the customers with long replies. You provide answers which are simple and straight to the point.  You should remember these things:
-        1. Your goal is to make the entire process of enquiry or booking at your hotel comfortable and hassle free for your customers.
-        2. Try to support the customer queries with appropriate amenities of the hotel wherever necessary to increase the chances of their booking.
-        3. Ask all necessary details while making bookings.
-        4. Summarize everything to the customer before making the booking
+        You are a Hotel Receptionist at "Four Points by Sheraton" hotel.
 
-        It is essential that you ALWAYS ask more than one questions BEFORE you provide an answer. This approach allows you to gain a comprehensive understanding of the customer's request and their unique situation.  
-        Most importantly you have to behave as HUMANLY as possible.  
-
-        You will be given a summary of the conversation made and a question, give the answer according to the rules given above.
-        Text: {summaries}
+        You will be given a context of the conversation made so far followed by a customer's question, 
+        give the answer to the question using the context. 
+        The answer should be short, straight and to the point.
+        If you don't know the answer, reply that the answer is not available.
+        Never Hallucinate.
+        
+        Context: {context}
 
         Question: {question}
         Answer:"""
 
         PROMPT = PromptTemplate(
-            template=prompt_template, input_variables=["summaries", "question"]
+            template=prompt_template, input_variables=["context", "question"]
         )
 
         chain_type_kwargs = { "prompt" : PROMPT }
-        llm = Cohere(model = "command", stop=["Question:"], temperature=0.2)
-        # print(PROMPT)
+        llm = Cohere(model = "command", temperature=0.5)
 
-
-        # llm_chain = LLMChain(prompt=prompt, llm=llm)
         #build your chain for RAG+C
-        st.session_state.chain = RetrievalQAWithSourcesChain.from_chain_type(     
-            llm=llm,
-            chain_type="stuff",
-            memory = ConversationSummaryMemory(llm = llm, memory_key='summaries', input_key='question', output_key= 'answer', return_messages=True),
-            retriever=retriever,
-            return_source_documents=False,
-            chain_type_kwargs=chain_type_kwargs
+        st.session_state.chain = ConversationalRetrievalChain.from_llm(     
+            llm = llm,
+            chain_type = "stuff",
+            memory = ConversationSummaryMemory(llm = llm, memory_key='chat_history', input_key='question', output_key= 'answer', return_messages=True),
+            retriever = vector_store.as_retriever(),
+           return_source_documents=False,
+           combine_docs_chain_kwargs=chain_type_kwargs,
+           
         )
-
-
-
 
 #Callblack function which when activated calls all the other
 #functions 
@@ -125,27 +127,23 @@ def on_click_callback():
     load_css()
     # print(st.session_state.customer_prompt)
     customer_prompt = st.session_state.customer_prompt
- 
-
 
     if customer_prompt:
+        
         st.session_state.input_value = ""
         st.session_state.initial_message_sent = True
+
         with st.spinner('Generating response...'):
 
             llm_response = st.session_state.chain(
                 {"question": customer_prompt,"summaries": st.session_state.chain.memory.buffer}, return_only_outputs=True)
             
-            print(st.session_state.chain.memory.buffer)
-            
+            # print(st.session_state.chain.memory.buffer)
             # answer = llm_response["answer"]
 
     st.session_state.history.append(
         Message("customer", customer_prompt)
     )
-    # print(Message)
-
-
     st.session_state.history.append(
         Message("AI", llm_response)
     )
@@ -158,9 +156,7 @@ prompt_placeholder = st.form("chat-form")
 creditcard_placeholder = st.empty()
 
 # query = st.chat_input()
-
 # if query!= None :
-
 #     st.session_state.customer_prompt = query
 #     # st.write(st.session_state.customer_prompt)
 #     result = st.session_state.chain(query)
@@ -180,26 +176,10 @@ with chat_placeholder:
             <img class="chatIcon" src = "app/static/{'elsa.png' if chat.origin == 'AI' else 'admin.png'}" width=32 height=32>
             <div class = "chatBubble {'adminBubble' if chat.origin == 'AI' else 'humanBubble'}">&#8203; {msg}</div>
         </div>"""
-
-
         st.markdown(div, unsafe_allow_html=True)
 
-# with prompt_placeholder : 
-#     cols = st.columns((6,1))
-#     cols[0].text_input(
-#         "Chat",
-#         value= "HI",
-#         label_visibility="collapsed",
-#         key = "customer_prompt",
-#     )
 
-#     cols[1].form_submit_button(
-#         "ask",
-#         type = "secondary",
-#         on_click = on_click_callback,
-#     )
-
-# Streamlit UI elements
+# Streamlit UI Input field
 with st.form(key="chat_form"):
     cols = st.columns((6, 1))
     
@@ -210,8 +190,7 @@ with st.form(key="chat_form"):
             placeholder="Hello, how can I assist you?",
             label_visibility="collapsed",
             key="customer_prompt",
-        )
-        
+        )  
     else:
         cols[0].text_input(
             "Chat",
